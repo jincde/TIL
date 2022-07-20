3d2185ef333438e8adf1278bf0302716a0bf1259
jincde <asdzxc@kakao.com>
2022-07-20 14:41:31 +0900

    2019.py 문자열로 출력 변경

--- KDT실습/SWEA/2019.py
+++ KDT실습/SWEA/2019.py
@@ -1,13 +1,9 @@
-# 더블더블
+# 더블더블 !!
 # 1부터 주어진 횟수까지 2를 곱한 값
 
 N = int(input())
 i = 2
-result = []
 
 for j in range(N+1):
     number = i ** j
-    result.append(number)
-    
-
-print(result)
+    print(number, end=" ")
