64a355f6345a691cc6b2e325ee00130469921a54
jincde <asdzxc@kakao.com>
2022-07-20 15:21:11 +0900

    1986.py

---
+++ KDT실습/SWEA/1986.py
@@ -0,0 +1,12 @@
+T = int(input())
+for tc in range(1,T+1):
+    a = int(input())
+    total = 0
+
+    for i in range(1,a+1):
+        if i % 2:
+            total += i
+        else:
+            total -=i
+    
+    print(f'#{tc} {total}')
