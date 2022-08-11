4bb4d7095aab148edd9e9e0a223e0738e334aa18
jincde <asdzxc@kakao.com>
2022-08-11 14:45:45 +0900

    6996 풀이

---
+++ ALGORITHM/1. BAEKJOON/2. 선택/6996/6996.py
@@ -0,0 +1,14 @@
+# 6996. 에너그램
+
+T = int(input())
+
+for _ in range(T):
+    a, b = map(str, input().split())
+
+    list_a = sorted(list(a)) # list를 왜 씌워야할까?
+    list_b = sorted(list(b))
+
+    if list_a == list_b:
+        print(f'{a} & {b} are anagrams.')
+    else:
+        print(f'{a} & {b} are NOT anagrams.')
