473679464a19033c35208167cda71ba23c8f0fa7
jincde <asdzxc@kakao.com>
2022-08-13 23:17:43 +0900

    25192 풀이

--- ALGORITHM/1. BAEKJOON/2. 선택/25192/25192.py
+++ ALGORITHM/1. BAEKJOON/2. 선택/25192/25192.py
@@ -1,18 +1,37 @@
 # 25192. 인사성 밝은 곰곰이
 
-from re import I
+N = int(input())
 
+cnt = 0
+dic = {}
 
-T = int(input())
+for _ in range(N):
+    user = input()
 
-arr = []
-for _ in range(T):
-    arr.append(input())
-    
-    for i in range(len(arr)):
-        if 'ENTER' in arr:
-            arr.pop()
-        else:
-            pass
+    if user == 'ENTER': # ENTER가 들어오면
+        for key, value in dic.items(): # 튜플 (key, value)
+            if value == 1: # value마다 카운트
+                cnt += 1
+        dic = {} # 딕셔너리 초기화
         
-print(len(set(arr)))
+    else:
+        if user not in dic: # user ID가 딕셔너리에 없다면
+            dic[user] = 1 # 딕셔너리에 추가
+
+for key, value in dic.items():
+    if value == 1:
+        cnt += 1
+
+print(cnt)
+    
+    
+'''
+7
+ENTER
+pjshwa
+chansol
+chogahui05
+ENTER
+pjshwa
+chansol
+'''
