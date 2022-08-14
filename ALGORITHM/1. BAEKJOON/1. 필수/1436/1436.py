87e6cba5c42f8f09639f04c8d722d838fb732cda
jincde <asdzxc@kakao.com>
2022-08-14 23:15:56 +0900

    1436 풀이

--- ALGORITHM/1. BAEKJOON/1. 필수/1436/1436.py
+++ ALGORITHM/1. BAEKJOON/1. 필수/1436/1436.py
@@ -1,5 +1,13 @@
 # 1436. 영화감독 숌
 
-N = input()
+N = int(input())
+first = 666
 
-print(N+'166')
+while N != 0:
+    if '666' in str(first):
+        N -= 1
+        if N == 0:
+            break
+    first += 1
+
+print(first)
