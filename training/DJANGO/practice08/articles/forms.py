a42d66d6da5b2dd4e499111aa52b4ce7f597c676
jincde <asdzxc@kakao.com>
2022-10-19 09:30:32 +0900

    Add: accounts/index

--- training/DJANGO/practice08/articles/forms.py
+++ training/DJANGO/practice08/articles/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from .models import Article
 
-class ArticleForm(forms.Form):
-  title = forms.CharField(max_length=21, error_messages={"required": "제목을 입력해주세요."}, label="제목")
-  content = forms.CharField(max_length=21, error_messages={"required": "내용을 입력해주세요."}, label="내용", widget=forms.Textarea) 
+class ArticleForm(forms.ModelForm):
+
+    class Meta:
+        model = Article
+        fields = ['title', 'content']
--- training/DJANGO/practice08/articles/views.py
+++ training/DJANGO/practice08/articles/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+from accounts.models import User
+
 
 # Create your views here.
 def index(request):
-  return render(request, 'articles/index.html')
+    articles = Article.objects.all().order_by('-pk')
+
+    return render(request, 'articles/index.html', {"articles": articles})
+
+def write(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.writer_id = request.user.id
+            f.save()
+
+            return redirect('articles:index')
+
+    else:
+        form = ArticleForm()
+
+    return render(request, 'articles/write.html', {'form': form})
